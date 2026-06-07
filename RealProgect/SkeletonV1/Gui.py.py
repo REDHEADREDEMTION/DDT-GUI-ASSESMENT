9a9cfe738eb3572a31922878303b25062a29a48c
REDHEADREDEMTION <jacokaalsen23@gmail.com>
2026-06-08 10:30:49 +1200

    I started to create my real app.

---
+++ RealProgect/SkeletonV1/Gui.py.py
@@ -0,0 +1,17 @@
+#import module
+from tkinter import *
+import tkinter as tk
+from PIL import Image, ImageTk
+
+#create root window
+root= tk.Tk()
+
+#root window title and dimensions
+root.title("Welcome to GeekForGeeks")
+#set geometry (width x hight)
+root.geometry('800x800')
+
+
+root.mainloop()
+
+
